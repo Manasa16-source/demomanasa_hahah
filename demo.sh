51d2bf54544eb2858f69224f7b3e6465f49ea0f4
Manasa16-source <115552074+Manasa16-source@users.noreply.github.com>
2022-12-03 14:53:27 +0530

    jenkins install on demo server

---
+++ demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ssh 'root@172.31.80.74'
+sudo su
+apt-get update
+apt install openjdk-11-jdk -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt update -y
+apt install jenkins -y
